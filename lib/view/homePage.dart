9044af30ff0889614653afb6f6d438284edb6b7a
Masaki Kono <masaki19931015@gmail.com>
2022-08-07 22:45:08 +0900

    done: homePage.dart

---
+++ lib/view/homePage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'widgets/calendarPageView.dart';
+
+class HomePage extends HookConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef, ref) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        //Todo：ドロップダウンリストを作成
+        //Expanded(child:),
+        Expanded(
+          flex: 8,
+          child: CalendarPageView(),
+        ),
+      ],
+    );
+  }
+}
