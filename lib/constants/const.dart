74a8f41f6e5e874180287818804d5722a4130e7d
Masaki Kono <masaki19931015@gmail.com>
2022-08-09 17:47:12 +0900

    done: const.dart

---
+++ lib/constants/const.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+const kMainColour = Color(0xFFFFFFFF);
+const kNuanceColour = Color(0xFFE0E0E0);
+const kAccentColour = Color(0xFFA0A0A0);
+const kBaseColour = Color(0xFF000000);
+
+const kNumberTextStyle = TextStyle(
+  color: kMainColour,
+  fontSize: 48.0,
+);
+
+const kWhiteTextStyle = TextStyle(
+  color: kMainColour,
+  fontSize: 18.0,
+);
+
+const kBlackTextStyle = TextStyle(
+  color: kBaseColour,
+  fontSize: 18.0,
+);
